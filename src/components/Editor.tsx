736c28a2a0cf71508b4681a8f6e86881aea91e75
kenkenman07 <ken0522g@icloud.com>
2026-04-14 17:27:10 +0900

    リマインダーのテーマを白に固定

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,6 @@
-import { useCreateBlockNote } from '@blocknote/react';
-import '@blocknote/mantine/style.css';
-import { BlockNoteView } from '@blocknote/mantine';
+import { useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/mantine/style.css";
+import { BlockNoteView } from "@blocknote/mantine";
 import { ja } from "@blocknote/core/locales";
 
 interface EditorProps {
@@ -12,11 +12,15 @@ function Editor({ onChange, initialContent }: EditorProps) {
   const editor = useCreateBlockNote({
     dictionary: ja,
     initialContent:
-    initialContent != null ? JSON.parse(initialContent) : undefined,
+      initialContent != null ? JSON.parse(initialContent) : undefined,
   });
   return (
     <div>
-      <BlockNoteView editor={editor} onChange={() => onChange(JSON.stringify(editor.document))}/>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => onChange(JSON.stringify(editor.document))}
+        theme="light"
+      />
     </div>
   );
 }
